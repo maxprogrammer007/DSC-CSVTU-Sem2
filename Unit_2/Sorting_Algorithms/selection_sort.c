f265542a37dbc89c70a3fd75776c34f2c69591ce
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-11 23:43:01 +0530

    implemented selection sort

---
+++ Unit_2/Sorting_Algorithms/selection_sort.c
@@ -0,0 +1,28 @@
+//
+// Created by Administrator on 11-Apr-24.
+//
+#include <stdio.h>
+
+int main(void){
+    int a[] = {5, 2, 4, 6, 1, 3};
+    int n = 6;
+    int i, j, temp, min;
+
+    for(i = 0; i < n - 1; i++){
+        min = i;
+        for(j = i + 1; j < n; j++){
+            if(a[j] < a[min]){
+                min = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+
+    for(i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
